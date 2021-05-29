9400ff927f87a97260477d3fd0435847bb4d60bc
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2021-05-30 02:57:10 +0700

    Add loadData function
    CarCatalog class can now load data from database

--- src/CarCatalog.py
+++ src/CarCatalog.py
@@ -1,6 +1,36 @@
+import sqlite3
+from sqlite3 import Error
+
+from src.CarItem import CarItem
+
+
 class CarCatalog:
     def __init__(self):
         self.carList = []
+        self.loadData()
+
+    def loadData(self):
+        try:
+            sqliteConnection = sqlite3.connect("../db/carCatalog.db")
+            print("Connected to SQLite")
+
+            sqlite_select_query = "SELECT * from carCatalog"
+            result = sqliteConnection.execute(sqlite_select_query)
+            for row in result:
+                # print(row)
+                image, brand, id, model, carClass, pph, available = row[0], row[2], row[1], row[3], row[4], row[5], row[6]
+                car = CarItem(image, brand, id, model, carClass, pph, available)
+                self.addCar(car)
+
+        except sqlite3.Error as error:
+            print("Failed to read data from sqlite table", error)
+        finally:
+            if sqliteConnection:
+                sqliteConnection.close()
+                print("The SQLite connection is closed")
+
+    def getList(self):
+        return self.carList
 
     def addCar(self, car):
         self.carList.append(car)
@@ -11,3 +41,8 @@ class CarCatalog:
     def displayCar(self):
         for i in self.carList:
             print(i)
+
+
+if __name__ == '__main__':
+    c = CarCatalog()
+    c.displayCar()
