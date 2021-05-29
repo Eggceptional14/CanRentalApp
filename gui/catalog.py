8f93c27d77cd5ee9eec8c1b20a1d17abbd3f1be6
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2021-05-30 02:58:12 +0700

    Update catalog page
    catalog page now display each car description

--- gui/catalog.py
+++ gui/catalog.py
@@ -2,8 +2,10 @@ import sys
 from PySide6.QtWidgets import *
 from PySide6.QtCore import *
 from PySide6.QtGui import *
+
 from gui.catalog_page import Ui_Form
 from src.CarCatalog import CarCatalog
+from gui.car_description import CarDescription
 
 import sqlite3
 from sqlite3 import Error
@@ -13,6 +15,22 @@ class Catalog(QWidget):
         QWidget.__init__(self, None)
         self.ui = Ui_Form()
         self.ui.setupUi(self)
+
+        self.vbox = QVBoxLayout()
+        self.hBox = QHBoxLayout()
+        self.carCatalog = CarCatalog()
+        self.carList = self.carCatalog.getList()
+        for car in self.carList:
+            if car.getAvailability() != 0:
+                description = CarDescription(car)
+                print(car)
+                self.hBox.addWidget(description)
+
+        self.vbox.addItem(self.hBox)
+        verticalSpacer = QSpacerItem(20, 80)
+        self.vbox.addSpacerItem(verticalSpacer)
+        self.ui.scrollAreaWidgetContents.setLayout(self.hBox)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
