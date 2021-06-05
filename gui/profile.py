0dbb43eadff6a19c5552ce88eb33ef1e1b05cbcc
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2021-06-05 19:15:16 +0700

    Add info display

--- gui/profile.py
+++ gui/profile.py
@@ -4,18 +4,29 @@ from PySide6.QtGui import *
 
 from gui.link.profile_ui import Ui_Form
 from gui.car_history import CarHistory
+from src.profile import Profile
 
 
-class Profile(QWidget):
+class ProfileUI(QWidget):
     def __init__(self):
         QWidget.__init__(self, None)
         self.ui = Ui_Form()
         self.ui.setupUi(self)
         self.ui.rentHsBtn.clicked.connect(self.showHistory)
+        self.user = ""
+        self.profile = Profile()
         self.history = CarHistory()
 
-    def setInfo(self, user):
-        pass
+    def setUser(self, user):
+        self.user = user
+        self.profile.loadData(self.user)
+
+    def setInfo(self):
+        self.ui.uNameLabel.setText(self.profile.uName)
+        self.ui.nameLabel.setText(self.profile.name)
+        self.ui.emailLabel.setText(self.profile.email)
+        self.ui.contactLabel.setText(self.profile.contact)
+        self.ui.cardLabel.setText(self.profile.credCard)
 
     def showHistory(self):
         self.history.show()
