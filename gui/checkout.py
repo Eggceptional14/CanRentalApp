857d58f7b9f1a75fb6e65aa82b25cf7b35e726c3
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2021-06-07 10:25:12 +0700

    Update checkout page information handling

--- gui/checkout.py
+++ gui/checkout.py
@@ -4,6 +4,9 @@ from PySide6.QtGui import *
 
 from gui.link.checkout_ui import Ui_Form
 
+import sqlite3
+from sqlite3 import Error
+
 
 class Checkout(QWidget):
     def __init__(self):
@@ -22,8 +25,69 @@ class Checkout(QWidget):
         self.ui.priceLabel.setText(str(self.car.getPrice()) + " ฿")
 
     def createOrder(self):
-        print("Checkout")
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Information)
 
+        message = "Reservation ID: {resvId}\n\
+                    From {sDate} to {rDate}\n\
+                    Car ID: {cId} {cBrand} {cModel}".format(resvId=self.getReservationId(), sDate=self.getStartDate(), rDate=self.getReturnDate(),
+                                                            cId=self.car.getCarId, cBrand=self.car.getBrand, cModel=self.car.getCarModel)
+
+        msg.setText("Checkout:")
+        msg.setWindowTitle("Checkout")
+        msg.setDetailedText(message)
+        msg.setStandardButtons(QMessageBox.Ok)
+
+        msg.exec_()
+
+    def getStartDate(self):
+        return self.ui.startInput.text()
+
+    def getReturnDate(self):
+        return self.ui.endInput.text()
+
+    def getReservationId(self):
+        conn = None
+        try:
+            conn = sqlite3.connect("../db/rentalCar.db")
+            cur = conn.cursor()
+            cur.execute("SELECT COUNT (*) FROM fixtures")
+            rowcount = cur.fetchone()[0]
+            if len(rowcount) == 0:
+                return
+            else:
+                return rowcount
+        except Error as e:
+            print(e)
+
+    def recordCarReservation(self, car, userName):
+        reservationId = self.getReservationId()
+        carId = car.getCarId()
+        startDate = self.getStartDate()
+        endDate = self.getReturnDate()
+
+        try:
+            sqliteConnection = sqlite3.connect("../db/userInfo.db")
+            cursor = sqliteConnection.cursor()
+            print("Connected to SQLite")
+
+            sqlite_insert_with_param = """INSERT INTO reservationHistory
+                            (reservationId, carId, userName, startDate, endDate) 
+                            VALUES (?, ?, ?, ?, ?);"""
+
+            data_tuple = (reservationId, carId, userName, startDate, endDate)
+            cursor.execute(sqlite_insert_with_param, data_tuple)
+            sqliteConnection.commit()
+            print("Inserted successfully")
+
+            cursor.close()
+
+        except sqlite3.Error as error:
+            print("Failed to insert Python variable into sqlite table", error)
+        finally:
+            if sqliteConnection:
+                sqliteConnection.close()
+                print("The SQLite connection is closed")
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
